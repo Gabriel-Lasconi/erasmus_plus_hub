e2636f067f10a6a44d6c8af45079b5b463e104fb
Gabriel Lasconi <Lasco@MacBook-Pro.local>
2025-02-05 09:47:04 +0200

    Fixed port binding for Render

--- hub/hub/settings.py
+++ hub/hub/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+PORT = os.getenv("PORT", "10000")  # Default to 10000 if PORT is not set
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
